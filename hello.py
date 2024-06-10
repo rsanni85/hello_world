4aa417254d080d3c71761c1d05f0615cf1e37cbe
rsanni85 <143829519+rsanni85@users.noreply.github.com>
2024-06-10 13:22:24 -0400

    Update hello.py on github_edits 2

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 print("This is my first Git repository on GitHub.")
 
 def print_message(name='GitHub'):
-  printf(f"Welcome to the {name} tutorial!")
+  print(f"Welcome to the {name} tutorial!")
 print_message()
